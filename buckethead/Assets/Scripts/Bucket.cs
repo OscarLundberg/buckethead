679afe723831f2448bda6f80702525cbab3d6316
Oscar Lundberg <info@oscarlundberg.com>
2022-11-12 22:17:30 +0100

    bucket multi fix

--- buckethead/Assets/Scripts/Bucket.cs
+++ buckethead/Assets/Scripts/Bucket.cs
@@ -6,15 +6,20 @@ public class Bucket : MonoBehaviour
 {
     public bool isBucket;
     public bool isFull;
+    public int capacity = 20;
+    public int current = 0;
     private void OnCollisionStay2D(Collision2D other)
     {
         var isWater = other.transform.tag == "water";
         if (isWater && isBucket)
         {
                
-            if(!isFull)
+            if(current < capacity)
             {
-                isFull = true;       // set bucket to full  
+                current++;
+                if(current >= capacity)
+                    isFull = true;       // set bucket to full  
+                    
                 Destroy(other.gameObject); // remove the water from the ground
             }
 
